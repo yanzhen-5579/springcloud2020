e5363ff28133989f104f297e148d274e90e41acc
yzz <yzz@qq.com>
2020-05-08 14:16:32 +0800

    p82

---
+++ cloud-config-center-3366/src/main/java/com/yanzhen/springcloud/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.yanzhen.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${config.info}")
+    String info;
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/info")
+    public String info(){
+        return info +"\t"+port;
+    }
+}
